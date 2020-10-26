9170c9abc7ced43113b8b502f5e58b0329fbbb0b
alfredwang0707 <alfredwang0707@gmail.com>
2020-10-26 19:04:07 -0400

    instance methods passed

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,12 @@
-Class Person
+class Person
  def talk
 puts "Hello World!"
+  def walk
+ puts "The Person is walking"
+  end
  end
 end
 
-def walk
- puts "The Person is walking"
-end
-
+Ada= Person.new
+Ada.talk
+Ada.walk
